755e34469ee98d6a573633fe19c11f97f6a392c6
luanaadn <luanaadn@hotmail.com>
2022-01-05 21:12:46 -0300

    Implementação de evento utilizando o método addEventListener.

--- assets/js/teste.js
+++ assets/js/teste.js
@@ -1,5 +1,7 @@
 var currentNumberWrapper = document.getElementById("currentNumber");
 var currentNumber = 0;
+window.addEventListener("load",msg1);
+
 
 function increment(){
     currentNumber = currentNumber + 1;
@@ -10,6 +12,11 @@ function decrement(){
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }  
+
+function msg1(){
+    alert("Seja bem-vindo(a) ao nosso contador online!");
+    }
+
 
 
 
